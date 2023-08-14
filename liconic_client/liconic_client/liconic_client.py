e059c341475f556ccc7f237146de134efaf34f99
Dozgulbas <dozgulbas@anl.gov>
2023-08-14 11:37:22 -0500

    fixes mispelling and json load issues

--- liconic_client/liconic_client/liconic_client.py
+++ liconic_client/liconic_client/liconic_client.py
@@ -126,7 +126,7 @@ class liconicNode(Node):
         elif request.action_handle == "set_target_temp":
             # collect variables 
             try:   
-                vars = json.loads(request.vars)
+                vars = eval(request.vars)
                 temp = float(vars.get('temp'))   # must be a float 
             except ValueError as error_msg:
                 response.action_response = -1
@@ -156,7 +156,7 @@ class liconicNode(Node):
         elif request.action_handle == "set_target_humidity": 
             # collect variables 
             try:         
-                vars = json.loads(request.vars)
+                vars = eval(request.vars)
                 humidity = float(vars.get('humidity'))  
             except ValueError as error_msg: 
                 response.action_response = -1
@@ -190,7 +190,7 @@ class liconicNode(Node):
             # TODO: make shaker speed a client variable so it doesn't have to be specified each time 
             # collect variables 
             try: 
-                vars = json.loads(request.vars)
+                vars = eval(request.vars)
                 new_shaker_speed = int(vars.get('shaker_speed')) 
             except ValueError as error_msg: 
                 response.action_response = -1
@@ -255,11 +255,10 @@ class liconicNode(Node):
         elif request.action_handle == "load_plate":
             print("Load plate called")  # TESTING
             try:
-                vars = json.loads(request.vars)
+                vars = eval(request.vars)
                 stacker = vars.get('stacker', None)
                 slot = vars.get('slot', None)
                 plate_id = vars.get('plate_id', None) # TODO: default plate id value?
-                print(f"plate id: {plate_id}")  # TESTING
             except ValueError as error_msg: 
                 response.action_response = -1
                 response.action_msg = "Error: stacker and slot variables must be integers"
@@ -296,7 +295,7 @@ class liconicNode(Node):
 
         elif request.action_handle == "unload_plate": 
             try: 
-                vars = json.loads(request.vars)
+                vars = eval(request.vars)
                 stacker = vars.get('stacker', None)
                 slot = vars.get('slot', None)
                 plate_id = vars.get('plate_id', None) # TODO: default plate id value?
@@ -402,7 +401,7 @@ def main(args = None):
     rclpy.init(args=args)  # initialize Ros2 communication
     node = liconicNode(NODE_NAME=NAME)
     rclpy.spin(node)     # keep Ros2 communication open for action node
-    rclpaction_handley.shutdown()     # kill Ros2 communication
+    rclpy.shutdown()     # kill Ros2 communication
 
 if __name__ == '__main__':
     main()
